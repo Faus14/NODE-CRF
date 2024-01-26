b277c71274687a4cd2d16c1121c430d1220a55d5
Faus14 <fausaludas14@gmail.com>
2024-01-26 03:05:36 -0300

    posta ULTasd

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,22 +1,11 @@
 const mongoose = require('mongoose');
-// Utiliza las variables de entorno proporcionadas por Railway
-const mongoURI = process.env.MONGO_URL || 'mongodb://mongo:e3-C2EH4Bca-fGEfHdde4a1-DGfBFbH2@172.26.0.2:27017/Aplicacion';
 
-const connectDB = async () => {
-    try {
-        const con = await mongoose.connect(mongoURI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+const mongoURI = 'mongodb://172.27.0.2:27017/Aplicacion';
 
-        console.log(`MongoDB connected: ${con.connection.host}`);
-    } catch (err) {
-        console.error(`Error connecting to MongoDB: ${err.message}`);
-        process.exit(1);
-    }
-};
-
-module.exports = connectDB;
-
-// Llama a la función connectDB
-connectDB();
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('MongoDB connected successfully');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
